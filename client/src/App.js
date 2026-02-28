33bb967d487507778462c87213ea52b2dd366b3c
Rajnish Sharma <sharma.rajnish091@gmail.com>
2026-02-28 16:46:50 +0530

    Remove Certifications and Trusted Institutions sections

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
 import Pricing from './components/Pricing';
-import Experience from './components/Experience';
-import Certifications from './components/Certifications';
 import Approach from './components/Approach';
 import Testimonials from './components/Testimonials';
 import Booking from './components/Booking';
@@ -25,8 +23,6 @@ function App() {
       <About />
       <Services />
       <Pricing />
-      <Experience />
-      <Certifications />
       <Approach />
       <Testimonials />
       <Blog />
